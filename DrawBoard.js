ef636c0f2b3788e5b4c9dcc8511acae2f067e544
刘少杰 <439655668@qq.com>
2018-01-27 11:05:38 +0800

    更改init接口

--- DrawBoard.js
+++ DrawBoard.js
@@ -1,11 +1,10 @@
 class DrawBoard {
-    constructor() {
-
+    constructor(div) {
+	    this._initCanvas(div)
     }
 
-    init(div) {
-        this._initCanvas(div)
-        this._initState()
+    init(opt) {
+        this._initState(opt)
         this._initBind()
     }
 
@@ -21,7 +20,12 @@ class DrawBoard {
         this.ctx = c.getContext('2d')
     }
 
-    _initState() {
+    _initState(opt) {
+        this.penSize = opt.pen.size
+        this.eraserSize = opt.eraser.w
+        this.eraserIco = opt.eraser.ico
+        this.maxStack = opt.maxStack
+        
         this.tool = 'pen' // 'eraser' 'no'
         this.isDown = false
         this.color = '#f00'
@@ -98,19 +102,23 @@ class DrawBoard {
     }
 
     _draw(last, p) {
-        let {ctx, color} = this
+        let {ctx, color, penSize} = this
         ctx.globalCompositeOperation = 'source-over'
-        this._lineTo(last, p, color, 5)
+        this._lineTo(last, p, color, penSize)
     }
 
     _eraser(last, p) {
-        let {ctx} = this
+        let {ctx, eraserSize} = this
         ctx.globalCompositeOperation = 'destination-out'
-        this._lineTo(last, p, '#00ff00', 20)
+        this._lineTo(last, p, '#00ff00', eraserSize)
     }
 
     setTool(tool) {
         if (-1 === ['pen', 'eraser', 'no'].indexOf(tool)) {return -1}
+        if (tool === 'eraser') {
+            console.log('er')
+            this.canvas.style.cursor = `url(${this.eraserIco}), auto`
+        }
         this.tool = tool
     }
 
@@ -119,14 +127,27 @@ class DrawBoard {
     }
 
     _save() {
+        let {history, maxStack} = this
         let image = this.ctx.getImageData(0, 0, this.w, this.h)
-        this.history.push(image)
-        console.log(image)
+        history.push(image)
+        if (history.length > maxStack) {
+	        history.shift()
+        }
     }
 
     undo() {
         let {ctx, w, h, history} = this
+        if (history.length === 0) {
+            console.log('no more stack')
+            return
+        }
         ctx.clearRect(0, 0, w, h)
         ctx.putImageData(history.pop(), 0, 0)
+    }
+    
+    clear() {
+	    let {ctx, w, h} = this
+        this._save()
+	    ctx.clearRect(0, 0, w, h)
     }
 }
